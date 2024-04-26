16253490c44d0bc70bee4bb0dc8a9851a06fa442
Leiko66 <landres8866@gmail.com>
2024-04-26 18:11:35 -0500

    Se agrega el proyecto

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import CoverParticles from "@/components/cover-particles";
+
+
+export default function Home() {
+  return (
+    <main>
+      <div className="flex min-h-[100vh] h-full bg-no-repeat bg-gradient-cover">
+        <CoverParticles />
+        <p>Introduccion</p>
+      </div>
+    </main>
+  );
+}
